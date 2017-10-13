c3df3538f2cb6cf4f1045411b80a384fc6b2d0cc
lihao <romcdy@163.com>
2017-10-13 14:15:27 +0800

    update

---
+++ sign/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from sign.models import Event, Guest
+# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status', 'address', 'start_time']
+    search_fields = ['name']
+    list_filter = ['status']
+
+class GusetAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone', 'email', 'sign', 'create_time', 'event']
+    search_fields = ['realname', 'phone']
+    list_filter = ['sign']
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GusetAdmin)
---
+++ sign/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from sign.models import Event, Guest
+#https://github.com/defnngj/guest
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+#登录界面
+def login(request):
+    return render(request, "login.html")
+
+#发布会管理
+@login_required
+def event_manage(request):
+    event_list = Event.objects.all()
+    username =request.session.get('user', '')                                                                           #读取浏览器session
+    return render(request, "event_manage.html", {"user": username, "events": event_list})
+
+#登录动作
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)                                                                                   #登录
+            request.session['user'] = username                                                                          #将session信息记录到浏览器
+            response = HttpResponseRedirect('/event_manage/')
+            return response
+        else:
+            return render(request, 'login.html', {'error': 'username or password error!'})
