c09d317220e98cdd505422d32cba2a0aad97480b
lihao <romcdy@163.com>
2017-10-16 17:44:31 +0800

    update

--- sign/views.py
+++ sign/views.py
@@ -82,3 +82,19 @@ def search_phone(request):
 def sign_index(request, eid):
     event =get_object_or_404(Event, id=eid)
     return render(request, 'sign_index.html', {'event': event})
+
+#Ç©µ½¶¯×÷
+@login_required
+def sign_index_action(request, eid):
+    event = get_object_or_404(Event, id=eid)
+    phone = request.POST.get('phone', '')
+    print(phone)
+    result =Guest.objects.filter(phone=phone)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'event id or phone error'})
+    result = Guest.objects.get(phone=phone, event_id=eid)
+    if result.sign:
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'user has sign in'})
+    else:
+        Guest.objects.filter(phone=phone, event_id=eid).update(sign='1')
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'sign in success', 'guest': result})
