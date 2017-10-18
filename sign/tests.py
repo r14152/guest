9dcae6cf2c63fc104f3460236ae56a87bef13659
lihao <romcdy@163.com>
2017-10-18 18:01:51 +0800

    update

--- sign/tests.py
+++ sign/tests.py
@@ -1,12 +1,14 @@
+ï»¿# -*- coding: utf-8 -*-
 from django.test import TestCase
-from sign.models import Event, Guest
 from django.contrib.auth.models import User
+from sign.models import Event, Guest
+
 # Create your tests here.
 class ModelTest(TestCase):
     def setUp(self):
         Event.objects.create(id=1, name="oneplus 3 event", status=True, limit=2000, address='shenzhen',
                              start_time='2017-10-13 17:08:22.000000')
-        Guest.objects.create(id=1, event_id=1,realname='alen', phone='13711001101', email='alen@mail.com',
+        Guest.objects.create(id=1, event_id=1, realname='alen', phone='13711001101', email='alen@mail.com',
                              sign=False)
 
     def test_event_models(self):
@@ -20,68 +22,55 @@ class ModelTest(TestCase):
         self.assertFalse(result.sign)
 
 class IndexPageTest(TestCase):
-    '''²âÊÔindexµÇÂ¼Ê×Ò³'''
     def test_index_page_renders_index_template(self):
-        '''²âÊÔindexÊÓÍ¼'''
         response = self.client.get('/index/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
 class LoginActionTest(TestCase):
-    '''²âÊÔµÇÂ¼¶¯×÷'''
-
     def setUp(self):
         User.objects.create_user('admin', 'admin@mail.com', 'admin123456')
 
     def test_add_admin(self):
-        '''²âÊÔÌí¼ÓÓÃ»§'''
         user = User.objects.get(username="admin")
         self.assertEqual(user.username, "admin")
         self.assertEqual(user.email, "admin@mail.com")
 
     def test_login_action_username_password_null(self):
-        '''ÓÃ»§ÃÜÂëÎª¿Õ'''
         test_data = {'username': '', 'password': ''}
         response = self.client.post('/login_action', data=test_data)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"username or password error!", response.content)
 
     def test_login_action_username_password_error(self):
-        '''ÓÃ»§ÃûÃÜÂë´íÎó'''
         test_data = {'username': 'abc', 'password': '123'}
         response = self.client.post('/login_action/', data=test_data)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"username or password error!", response.content)
 
     def test_login_action_success(self):
-        '''µÇÂ½³É¹¦'''
         test_data = {'username': 'admin', 'password': 'admin123'}
         response = self.client.post('/login_action', data=test_data)
         self.assertEqual(response.status_code, 302)
 
 class EventMangeTest(TestCase):
-    ''' ·¢²¼»á¹ÜÀí '''
-
     def setUp(self):
         User.objects.create_user('admin', 'admin@mail.com', 'admin123456')
         Event.objects.create(name="xiaomi5", limit=2000, address='beijing', status=1, start_time='2017-8-10 12:30:00')
         login_user = {'username': 'admin', 'password': 'admin123456'}
-        self.client.post('/login_action/', data=login_user)  # Ô¤ÏÈµÇÂ¼
+        self.client.post('/login_action/', data=login_user)
 
     def test_add_event_data(self):
-        ''' ²âÊÔÌí¼Ó·¢²¼»á '''
         event = Event.objects.get(name="xiaomi5")
         self.assertEqual(event.address, "beijing")
 
     def test_event_mange_success(self):
-        ''' ²âÊÔ·¢²¼»á:xiaomi5 '''
         response = self.client.post('/event_manage/')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"xiaomi5", response.content)
         self.assertIn(b"beijing", response.content)
 
     def test_event_mange_search_success(self):
-        ''' ²âÊÔ·¢²¼»áËÑË÷ '''
         response = self.client.post('/search_name/', {"name": "xiaomi5"})
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"xiaomi5", response.content)
@@ -89,40 +78,33 @@ class EventMangeTest(TestCase):
 
 
 class GuestManageTest(TestCase):
-    ''' ¼Î±ö¹ÜÀí '''
-
     def setUp(self):
         User.objects.create_user('admin', 'admin@mail.com', 'admin123456')
         Event.objects.create(id=1,name="xiaomi5", limit=2000, address='beijing', status=1, start_time='2017-8-10 12:30:00')
         Guest.objects.create(realname="alen", phone=18611001100,email='alen@mail.com', sign=0, event_id=1)
         login_user = {'username': 'admin', 'password': 'admin123456'}
-        self.client.post('/login_action/', data=login_user)  # Ô¤ÏÈµÇÂ¼
+        self.client.post('/login_action/', data=login_user)
 
     def test_add_guest_data(self):
-        ''' ²âÊÔÌí¼Ó¼Î±ö '''
         guest = Guest.objects.get(realname="alen")
         self.assertEqual(guest.phone, "18611001100")
         self.assertEqual(guest.email, "alen@mail.com")
         self.assertFalse(guest.sign)
 
     def test_event_mange_success(self):
-        ''' ²âÊÔ¼Î±öĞÅÏ¢: alen '''
         response = self.client.post('/guest_manage/')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"alen", response.content)
         self.assertIn(b"18611001100", response.content)
 
     def test_guest_mange_search_success(self):
-        ''' ²âÊÔ¼Î±öËÑË÷ '''
-        response = self.client.post('/search_phone/',{"phone":"18611001100"})
+        response = self.client.post('/search_phone/', {"phone": "18611001100"})
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"alen", response.content)
         self.assertIn(b"18611001100", response.content)
 
 
 class SignIndexActionTest(TestCase):
-    ''' ·¢²¼»áÇ©µ½ '''
-
     def setUp(self):
         User.objects.create_user('admin', 'admin@mail.com', 'admin123456')
         Event.objects.create(id=1, name="xiaomi5", limit=2000, address='beijing', status=1, start_time='2017-8-10 12:30:00')
@@ -133,25 +115,21 @@ class SignIndexActionTest(TestCase):
         self.client.post('/login_action/', data=login_user)
 
     def test_sign_index_action_phone_null(self):
-        ''' ÊÖ»úºÅÎª¿Õ '''
         response = self.client.post('/sign_index_action/1/', {"phone": ""})
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"phone error.", response.content)
 
     def test_sign_index_action_phone_or_event_id_error(self):
-        ''' ÊÖ»úºÅ»ò·¢²¼»áid´íÎó '''
         response = self.client.post('/sign_index_action/2/', {"phone": "18611001100"})
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"event id or phone error.", response.content)
 
     def test_sign_index_action_user_sign_has(self):
-        ''' ÓÃ»§ÒÑÇ©µ½ '''
         response = self.client.post('/sign_index_action/2/', {"phone": "18611001101"})
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"user has sign in.", response.content)
 
     def test_sign_index_action_sign_success(self):
-        ''' Ç©µ½³É¹¦ '''
         response = self.client.post('/sign_index_action/1/', {"phone": "18611001100"})
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"sign in success!", response.content)
